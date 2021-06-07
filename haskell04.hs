2e43c88c7467237f55d1028bef1d4eeb955abe52
Gabriel Heisler <gvheisler@inf.ufsm.br>
2021-06-07 16:23:33 -0300

    update

--- haskell04.hs
+++ haskell04.hs
@@ -16,8 +16,11 @@ classifIdosos tpl = [(name, age, faixaIdoso age)| (name, age) <- tpl]
 classifIdosos' :: [(String,Int)] -> [(String,Int,String)]
 classifIdosos' tpl = map (\(name, age)->(name, age, faixaIdoso age)) tpl
 
---strColor :: (Int,Int,Int) -> String
---strColor rgb = "rgb" ++ 
+strColor :: (Int,Int,Int) -> String
+strColor (r, g, b) =  "rgb(" ++ show r ++ ", " ++ show g ++ ", " ++ show b ++ ")"
 
---genCircs :: Int -> (Int,Int) -> Int -> [(Int,Int,Int)]
---genCircs
+genCircs :: Int -> (Int,Int) -> Int -> [(Int,Int,Int)]
+genCircs n (cx, cy) r = [(nx, cy, r) | nx <- take n [cx, cx+r*2..]]
+
+genReds :: Int -> [(Int, Int, Int)]
+genReds n = [(80+x*10, 0, 0) | x <- [0..n-1]]
